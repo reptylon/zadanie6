ee3ee244a207c18ce06de91d90233a438a94158b
reptylon <73425107+reptylon@users.noreply.github.com>
2021-01-17 19:10:46 +0100

    Update zadanie6.py

--- zadanie6.py
+++ zadanie6.py
@@ -1,6 +1,6 @@
+import copy
 di = {'one': [1], 'two': [2], 'three': [3], 'four': [4]}
-diCopied= di.copy()
-di['four'] = 'cztery'
-
+diCopied = copy.deepcopy(di)
+di['four'][0] = 'cztery'
 print(di)
 print(diCopied)
